7f35becf5814c018313a21d0c3db3412595ec433
Jorge Yagüe París <jorgeyp@gmail.com>
2019-03-03 20:27:02 +0100

    Fix Schema

--- models/rsvp.js
+++ models/rsvp.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose"),
-    Schema   = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 // api_version:    // 2
 var rsvpSchema = new Schema({
@@ -8,14 +8,14 @@ var rsvpSchema = new Schema({
         event_id: String, // Unique alphanumeric identifier
         event_name: String, // Name of the event
         event_url: String, // URL to the full event page
-        time: long, // Event time if set in milliseconds since the epoch
+        time: Date, // Event time if set in milliseconds since the epoch
     },
     group: { // Group hosting the event
         group_city: String, // Group's home city
         group_country: String, // two-letter code of group's home country
-        group_id: int, // Numeric identifier of the group
-        group_lat: double, // Latitude of group's approximate location
-        group_lon: double, // Longitude of group's approximate location
+        group_id: Number, // Numeric identifier of the group
+        group_lat: Number, // Latitude of group's approximate location
+        group_lon: Number, // Longitude of group's approximate location
         group_name: String, // -
         group_state: String, // two-letter code of group's home state, if in US or CA
         group_topics: [{ // Topics associated with this group
@@ -24,22 +24,33 @@ var rsvpSchema = new Schema({
         }],
         group_urlname: String, // Unique portion of group's URL, no slashes
     },
-    guests: int, // Number of guests the member is bringing
+    guests: Number, // Number of guests the member is bringing
     member: {    // Member who RSVP'd
-        member_id: int, // Unique numeric id
+        member_id: Number, // Unique numeric id
         member_name: String, // Full name given
         other_services: Schema.Types.Mixed, // e.g. {"twitter": {"identifier": "MeetupAPI"}}
         photo: String, // Thumbnail URL for member photo if one exists
     },
-    mtime: long, // Last modified time of this RSVP, in milliseconds since the epoch
+    mtime: Number, // Last modified time of this RSVP, in milliseconds since the epoch
     response: String, // "yes" or "no"
-    rsvp_id: int, // Unique numeric identifier
+    rsvp_id: Number, // Unique numeric identifier
     venue: { // Venue, if public
-        lat: double, // Latitude of the venue
-        lon: double, // Longitude of the venue
-        venue_id: int, // Unique numeric identifier
+        lat: Number, // Latitude of the venue
+        lon: Number, // Longitude of the venue
+        venue_id: Number, // Unique numeric identifier
         venue_name: String, // -
     },
-});
+    group_location: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    }
+}, {collection: 'rsvp'});
 
 module.exports = mongoose.model("Rsvp", rsvpSchema);
